3bf769f4c36033b1de0c97e1628b812c38e7f6e7
Ch4rIes <55766787+Ch4rIes@users.noreply.github.com>
2021-09-29 20:29:25 -0700

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -4,6 +4,6 @@ import java.util.*;
 public class Driver {
     public static void main(String[] args){
         Calculator c = new Calculator();
-        c.display(1 , 5 , 6 , 0 , 2 , 0 , 3 , 1 , 1 , 5 , 1 , 2 , 3);
+        c.display();
     }
 }
