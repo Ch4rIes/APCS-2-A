1b2175ddd7e26e3702496ddb32b85d7c1d532507
Ch4rIes <55766787+Ch4rIes@users.noreply.github.com>
2021-09-29 20:29:10 -0700

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,14 +1,14 @@
 public class Calculator {
-
     public double slope(double x1 , double x2 , double y1 , double y2){
+        // x1 , y1 are coordinates of the first point
+        // x2 , y2 are coordinates of the second point
+        double y_diff = y2 - y1;
         // y_diff represents y coordinate's difference
+        double x_diff = x2 - x1;
         // x_diff represents x coordinate's difference
         // the slope is y_diff / x_diff
-        double y_diff = y2 - y1;
-        double x_diff = x2 - x1;
         return y_diff / x_diff;
     }
-
     public double quadratic_formula_adding(double a , double b , double c){
         // a is the coefficient of X^2
         // b is the coefficient of X
@@ -55,21 +55,52 @@ public class Calculator {
         // the "sum" is the sum of the geometric series
         return sum;
     }
-    public void display(double a , double b , double c , double mpx1 , double mpx2 , double mpy1 , double mpy2 , double ar1 , double ardiff , double ark , double gf , double gr , double gk){
+    public void display(){
         // quadratic formula
+        int a = 1 , b = 5 , c = 6;
+        // a is the coefficient of X^2
+        // b is the coefficient of X
+        // c is the coefficient of 1
         System.out.println("QUADRATIC FORMULA");
         System.out.println("The solutions for " + a + " x^2 + " + b + "x +" + c +" are " + quadratic_formula_adding(a , b ,c) + " and " + quadratic_formula_sub(a , b , c));
+        
+        
         // slope formula
+        int x1 = 0 , y1 = 0 , x2 = 2 , y2 = 3;
+        // (x1 , y1) are the coordinate of the first point
+        // (x2 , y2) are the coordinate of the second point
         System.out.println("SLOPE FORMULA");
-        System.out.println("A line connecting the points (" + mpx1 +", " + mpy1 +") and (" + mpx2 +", " + mpy2 +") has a slope of "+ slope(mpx1 , mpx2 , mpy1 ,mpy2));
+        System.out.println("A line connecting the points (" + x1 +", " + y1 +") and (" +x2 +", " + y2 +") has a slope of "+ slope(x1 , x2 , y1 ,y2));
+        
+        
         // midpoint formula
+        x1 = 0;
+        y1 = 0;
+        x2 = 2;
+        y2 = 3;
+        // (x1 , y1) are the coordinate of the first point
+        // (x2 , y2) are the coordinate of the second point
         System.out.println("MIDPOINT FORMULA");
-        System.out.println("The midpoint between ("+ mpx1 +", " + mpy1 +") and (" + mpx2 +", " +mpy2 +") " + "is " + "(" + mid_point_X_cordindate(mpx1 , mpx2) + ", "+ mid_point_Y_cordindate(mpy1 , mpy2) + ")");
+        System.out.println("The midpoint between ("+ x1 +", " + y1 +") and (" + x2 +", " +y2 +") " + "is " + "(" + mid_point_X_cordindate(x1 , x2) + ", "+ mid_point_Y_cordindate(y1 , y2) + ")");
+        
+        
         // arithmetic
+        double f  = 1, diff  = 1, k = 5;
+        // f is the first term of the arithmetic series
+        // diff is the difference between terms
+        // k is the number of terms in the series
         System.out.println("SUM OF AN ARITHMETIC SERIES");
-        System.out.println("The sum of the first 5 terms of an arithmetic series that starts with " + ar1 + " and increases by " + ardiff + " is " + arithmetic(ar1 , ardiff , ark));
+        System.out.println("The sum of the first " + (int)k + " terms of an arithmetic series that starts with " + f + " and increases by " + diff + " is " + arithmetic(f , diff , k));
+        
+        
         // geometric sum
-        System.out.println("SUM OF AN GEOMETRIC SERIES");
-        System.out.println("The sum of the first " + gk + " terms of an geometric series that starts with " + gf + " and increases by a rate of " + gr + " is " + geometric(gf , gr ,gk));
+        double g1 = 3;
+        k = 3;
+        double r = 2;
+        // g1 is the first term of the geometric series
+        // r is the rate of change between terms
+        // k is the number of cases we will calculate sum
+        System.out.println("SUM OF A FINITE GEOMETRIC SERIES");
+        System.out.println("The sum of the first " + (int)k + " terms of an geometric series that starts with " + g1 + " and increases by a rate of " + r + " is " + geometric(g1 , r ,k));
     }
 }
