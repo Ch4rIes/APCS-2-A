d08ab020fd65312199a5fa2f0c4aaa5a1e0b76fe
Ch4rIes <55766787+Ch4rIes@users.noreply.github.com>
2021-09-29 20:33:26 -0700

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,3 +1,4 @@
+
 public class Calculator {
     public double slope(double x1 , double x2 , double y1 , double y2){
         // x1 , y1 are coordinates of the first point
@@ -63,16 +64,16 @@ public class Calculator {
         // c is the coefficient of 1
         System.out.println("QUADRATIC FORMULA");
         System.out.println("The solutions for " + a + " x^2 + " + b + "x +" + c +" are " + quadratic_formula_adding(a , b ,c) + " and " + quadratic_formula_sub(a , b , c));
-        
-        
+
+
         // slope formula
         int x1 = 0 , y1 = 0 , x2 = 2 , y2 = 3;
         // (x1 , y1) are the coordinate of the first point
         // (x2 , y2) are the coordinate of the second point
-        System.out.println("SLOPE FORMULA");
+        System.out.println("\nSLOPE FORMULA");
         System.out.println("A line connecting the points (" + x1 +", " + y1 +") and (" +x2 +", " + y2 +") has a slope of "+ slope(x1 , x2 , y1 ,y2));
-        
-        
+
+
         // midpoint formula
         x1 = 0;
         y1 = 0;
@@ -80,19 +81,19 @@ public class Calculator {
         y2 = 3;
         // (x1 , y1) are the coordinate of the first point
         // (x2 , y2) are the coordinate of the second point
-        System.out.println("MIDPOINT FORMULA");
+        System.out.println("\nMIDPOINT FORMULA");
         System.out.println("The midpoint between ("+ x1 +", " + y1 +") and (" + x2 +", " +y2 +") " + "is " + "(" + mid_point_X_cordindate(x1 , x2) + ", "+ mid_point_Y_cordindate(y1 , y2) + ")");
-        
-        
+
+
         // arithmetic
         double f  = 1, diff  = 1, k = 5;
         // f is the first term of the arithmetic series
         // diff is the difference between terms
         // k is the number of terms in the series
-        System.out.println("SUM OF AN ARITHMETIC SERIES");
+        System.out.println("\nSUM OF AN ARITHMETIC SERIES");
         System.out.println("The sum of the first " + (int)k + " terms of an arithmetic series that starts with " + f + " and increases by " + diff + " is " + arithmetic(f , diff , k));
-        
-        
+
+
         // geometric sum
         double g1 = 3;
         k = 3;
@@ -100,7 +101,7 @@ public class Calculator {
         // g1 is the first term of the geometric series
         // r is the rate of change between terms
         // k is the number of cases we will calculate sum
-        System.out.println("SUM OF A FINITE GEOMETRIC SERIES");
+        System.out.println("\nSUM OF A FINITE GEOMETRIC SERIES");
         System.out.println("The sum of the first " + (int)k + " terms of an geometric series that starts with " + g1 + " and increases by a rate of " + r + " is " + geometric(g1 , r ,k));
     }
 }
