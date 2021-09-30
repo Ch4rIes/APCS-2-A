b6bafeb69cd4344c5f941281b68a9626004be5d4
Ch4rIes <55766787+Ch4rIes@users.noreply.github.com>
2021-09-29 20:42:55 -0700

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,9 +1,12 @@
+//NAME: CHARLES ZUO
+//DATE: SEPT 29th
+//PROBLEM SET: 2-A
 import java.util.*;
-//author: Charles Zuo
 
 public class Driver {
     public static void main(String[] args){
         Calculator c = new Calculator();
+        // c is an instance of Calculator class
         c.display();
     }
 }
